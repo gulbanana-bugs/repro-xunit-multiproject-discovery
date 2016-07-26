bd41332c4536d7e11ca0126fb0f1748bf3b674b7
Thomas Castiglione <castiglione@mac.com>
2016-07-26 19:58:00 +0800

    Add project files.

---
+++ repro.bottom/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repro.bottom
+{
+    public class Class1
+    {
+    }
+}
